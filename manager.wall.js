c53dae1a01ee8f172e336d566623401cb9afff85
phkaaing <phkaaing@gmail.com>
2026-07-22 23:12:14 +0900

    fixed: 벽을 world 로 수정

--- manager.wall.js
+++ manager.wall.js
@@ -3,77 +3,22 @@ export class WallManager {
       this.scene = scene
       this.matter = this.scene.matter
       this.thickness = 40
-      this.wallLength = 50000
-
-      this.walls = this.initWalls()
     }
     create() {
-        this.destroy()
-
-        const currentSceneScale = this.getCurrentSceneSacle()
-        const width = currentSceneScale.width
-        const height = currentSceneScale.height
-        const thickness = this.thickness
-        const wallLength = this.wallLength
-
-        // 이미 생성되어 있다면 생성하지 않음
-        if (this.walls.floor) {
-          return
-        }
-
-        // 바닥
-        this.walls.floor = this.createWall(width / 2, height + (thickness / 2), wallLength, thickness, { isStatic: true})
-        // 천장
-        this.walls.ceiling = this.createWall(width / 2, -thickness / 2, wallLength, thickness, { isStatic: true})
-        // 왼쪽 벽
-        this.walls.left = this.createWall(-thickness / 2, height / 2, thickness, wallLength, { isStatic: true})
-        // 오른쪽 벽
-        this.walls.right = this.createWall(width + thickness / 2, height / 2, thickness, wallLength, { isStatic: true})
-    }
-    destroy() {
-      for (const wall of Object.values(this.walls)) {
-          if (wall) {
-            this.scene.matter.world.remove(wall)
-          }
-      }
-
-      this.walls = this.initWalls()
-    }
-    createWall(x, y, width, height, options) {
-      return this.matter.add.rectangle(
-        x,
-        y,
-        width,
-        height,
-        options
-      )
+      this.updateWall()
     }
     resize() {
-      const currentSceneScale = this.getCurrentSceneSacle()
-      const width = currentSceneScale.width
-      const height = currentSceneScale.height
-      const thickness = this.thickness
-
-      this.updatePosition(this.walls.floor, width / 2, height + (thickness / 2))
-      this.updatePosition(this.walls.ceiling, width / 2, -thickness / 2)
-      this.updatePosition(this.walls.left, -thickness / 2, height / 2)
-      this.updatePosition(this.walls.right, width + (thickness / 2), height / 2)
+      this.updateWall()
     }
-    updatePosition(wall, chageX, changeY) {
-      this.matter.body.setPosition(wall, { x: chageX, y: changeY })
+    updateWall() {
+      const currentSceneScale = this.getCurrentSceneSacle()
+    
+      this.matter.world.setBounds(0, 0, currentSceneScale.width, currentSceneScale.height, this.thickness)
     }
     getCurrentSceneSacle() {
       return {
         width: this.scene.scale.width,
         height: this.scene.scale.height
-      }
-    }
-    initWalls() {
-      return {
-        floor: null,
-        ceiling: null,
-        left: null,
-        right: null
       }
     }
 }
